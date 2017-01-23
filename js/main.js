a2fd02d305b3dbf4ec273fa7fee6424c3cce6e3f
Zeldina Chalco <dazelcolab2016@gmail.com>
2017-01-23 18:05:44 -0500

    run -twitter

--- js/main.js
+++ js/main.js
@@ -1,5 +1,64 @@
 
-var textLista=document.getElementById("textLista");
+var listaMensajes = document.getElementById("lista-mensajes");
+
+function crearNodos(container, mensaje){
+    var area = document.createElement('div');
+    area.className = "list , checkbox ,option animated zoomInDown";
+    var tacho = document.createElement('i');
+    tacho.className = "fa  fa-trash-o  tacho";
+    var label = document.createElement('label');
+    label.className = "text-justify";
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    area.appendChild(label);
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode(mensaje));
+    area.appendChild(tacho);
+    container.appendChild(area);
+    tacho.addEventListener('click',eliminaNodos);
+    checkbox.addEventListener('click',tachar);
+}
+
+function listaTareas()
+{
+    var mensaje=document.getElementById("mensaje");
+        if((mensaje.length) == 0 || /^\s*$/.test(mensaje.value))
+            {
+            alert ("Ingresa un mensaje");
+        }
+        else{
+            crearNodos(listaMensajes,mensaje.value);
+            
+        }
+        mensaje.focus();
+        mensaje.value="";
+}
+    
+function eliminaNodos(evt)
+{
+	evt.target.parentNode.className= "list , checkbox , animated zoomOutDown"; 
+  	setTimeout(function(){
+    listaMensajes.removeChild(evt.target.parentNode);  
+  	}, 1000);  
+}
+function tachar(evt)
+{
+    var check=evt.target.parentNode;
+    	var check=evt.target.parentNode;
+	if(evt.target.checked)
+		check.setAttribute("style","text-decoration:line-through");
+	else
+		check.setAttribute("style","text-decoration:none");
+}
+
+
+
+
+
+
+
+
+/*var textLista=document.getElementById("textLista");
 
 
 function listaTareas()
@@ -9,5 +68,4 @@ function listaTareas()
     contenido.innerHTML=textArea;
     textLista.appendChild(contenido);
     textArea.getElementById("textArea").value=" ";
-}
-
+}*/
